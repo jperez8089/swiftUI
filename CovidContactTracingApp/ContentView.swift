1d2ba6c4bb3faccf1fc485f45894ae2288ba4397
Javier Perez <javier_perez95@icloud.com>
2021-04-15 04:24:20 -0500

    Initial Commit

---
+++ CovidContactTracingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidContactTracingApp
+//
+//  Created by Javier Perez on 4/15/21.
+//  Copyright © 2021 Javier Perez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
